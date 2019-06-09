7e1a223ac482eaa52a4b094a8ce3127060a1029d
huhuaheng <48820771+huhuaheng@users.noreply.github.com>
2019-06-09 17:19:43 +0800

    提交

---
+++ src/test/java/com/wip/Demo.java
@@ -0,0 +1,23 @@
+package com.wip;
+
+import org.junit.Test;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018-12-03
+ * @Description:
+ */
+public class Demo {
+
+    public static void change(int age){
+        age = 20 ;
+    }
+
+    public static void main(String[] args) {
+        int age = 10 ;
+        change(age);
+        System.out.println(age);
+    }
+
+
+}
